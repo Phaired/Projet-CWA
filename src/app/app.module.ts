4d1d5e63ce7d4cfbe8970e657d5d49fda6b48bc4
Phaired <65019388+Phaired@users.noreply.github.com>
2023-10-16 11:39:18 +0200

    tailwind custom and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [AppComponent, HomeComponent, ContactComponent],
+    declarations: [AppComponent, HomeComponent, ContactComponent, FooterComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
