2b94e8b4cc6053b057c5eafe8a01679e0cff59ac
Phaired <65019388+Phaired@users.noreply.github.com>
2023-10-23 13:21:29 +0200

    component datepicker

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,4 +5,13 @@ import { Component } from '@angular/core';
     templateUrl: './contact.component.html',
     styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {}
+export class ContactComponent {
+    ContactList = [
+        { name: 'Yoan DUSOLEIL', github: 'https://github.com/Yaon-C2H8N2' },
+        { name: 'Rémy BARRANCO', github: 'https://github.com/Phaired' },
+        { name: 'Maxime COLLIAT', github: 'https://github.com/Maxime-Cllt' },
+        { name: 'Rahman YILMAZ', github: 'https://github.com/Sudo-Rahman' },
+        { name: 'Betul SENER', github: 'https://github.com/BetulDSENER' },
+        { name: 'Julie PRIGENT', github: 'https://github.com/JuliePrigent' },
+    ];
+}
