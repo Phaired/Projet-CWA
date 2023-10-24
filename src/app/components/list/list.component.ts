9930f9c1b43ed6035e4398c11266d48787ff0384
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2023-10-24 11:17:45 +0200

    issue #34 Ajout du component task dans le Home Ajout des vraies tasks dans le Home

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,108 @@
+import { Component, Renderer2 } from '@angular/core';
+import { LocalStorageRepositoryService } from '../../repository/local-storage-repository.service';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+@Component({
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.css'],
+    animations: [
+        trigger('prevCardAnimation', [
+            transition(':leave', [
+                style({ transform: 'translateX(0)', opacity: 1 }),
+                animate(
+                    '0.7s ease-in-out',
+                    style({ transform: 'translateX(-100vw)', opacity: 0 }),
+                ),
+            ]),
+            transition('* => left', [
+                style({ transform: 'translateX(-100vw)', opacity: 0 }),
+                animate(
+                    '0.7s ease-in-out',
+                    style({ transform: 'translateX(0)', opacity: 1 }),
+                ),
+            ]),
+        ]),
+        trigger('nextCardAnimation', [
+            transition(':leave', [
+                style({ transform: 'translateX(0)', opacity: 1 }),
+                animate(
+                    '0.7s ease-in-out',
+                    style({ transform: 'translateX(100vw)', opacity: 0 }),
+                ),
+            ]),
+            transition('* => right', [
+                style({ transform: 'translateX(100vw)', opacity: 0 }),
+                animate(
+                    '0.7s ease-in-out',
+                    style({ transform: 'translateX(0)', opacity: 1 }),
+                ),
+            ]),
+        ]),
+    ],
+})
+export class ListComponent {
+    protected task_list: any;
+
+    constructor(
+        private local_storage: LocalStorageRepositoryService,
+        private renderer: Renderer2,
+    ) {
+        this.task_list = this.local_storage
+            .getLocalStorageRepository()
+            .getAllTaches();
+
+        // this.task_list = [];
+    }
+
+    // ngOnInit() {
+    //     this.task_list = this.local_storage.getLocalStorageRepository().getAllTaches();
+    // }
+
+    itemsPerPage: number = 4;
+    currentPage: number = 1;
+
+    get paginatedTasks(): any[] {
+        const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+        return this.task_list.slice(startIndex, startIndex + this.itemsPerPage);
+    }
+
+    pageChanged(newPage: number): void {
+        this.currentPage = newPage;
+    }
+
+    get startIndex(): number {
+        return (this.currentPage - 1) * this.itemsPerPage;
+    }
+
+    get endIndex(): number {
+        return this.startIndex + this.itemsPerPage;
+    }
+
+    firstPage(): void {
+        this.currentPage = 1;
+    }
+
+    protected showCurrentCards: boolean = true;
+    protected showNewCards: boolean = false;
+
+    previousPage(): void {
+        if (this.currentPage > 1) {
+            this.currentPage--;
+            this.showNewCards = false;
+            setTimeout(() => {
+                this.showCurrentCards = true;
+            }, 700);
+        }
+    }
+
+    nextPage(): void {
+        if (this.endIndex < this.task_list.length) {
+            this.currentPage++;
+            this.showCurrentCards = false;
+            setTimeout(() => {
+                this.showNewCards = true;
+            }, 700);
+        }
+    }
+}
