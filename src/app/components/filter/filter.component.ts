ef86823a0576567da4332e2f7eb4dd7212d1b744
Yoan Dusoleil <y.dusoleil@outlook.com>
2023-11-07 14:03:55 +0100

    Issue #53 et plus encore

---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,108 @@
+import { Component, Input } from '@angular/core';
+import { Tache } from '../../model/Tache';
+
+@Component({
+    selector: 'app-filter',
+    templateUrl: './filter.component.html',
+    styleUrls: ['./filter.component.css'],
+})
+export class FilterComponent {
+    @Input() task_list: Tache[] = [
+        new Tache(1337, 'test', new Date(), new Date(), 'todo', 1, false, ''),
+    ];
+
+    constructor() {}
+
+    updateFilter(
+        orderBy: string,
+        status: string,
+        priority: string,
+        order: number,
+    ) {
+        this.filterByStatus(status);
+        this.filterByPriority(priority);
+        this.orderBy(orderBy, order);
+        console.log(this.task_list);
+    }
+
+    private orderBy(orderType: string, order: number) {
+        switch (orderType) {
+            case 'date': {
+                this.task_list.sort((a: Tache, b: Tache) => {
+                    const dateA = new Date(a.date_fin);
+                    const dateB = new Date(b.date_fin);
+                    return (dateA.getTime() - dateB.getTime()) * order;
+                });
+                break;
+            }
+            case 'priority': {
+                this.task_list.sort((a: Tache, b: Tache) => {
+                    return (a.priority - b.priority) * order;
+                });
+                break;
+            }
+            default: {
+                this.task_list.sort((a: Tache, b: Tache) => {
+                    return (a.id - b.id) * order;
+                });
+                break;
+            }
+        }
+    }
+
+    private filterByStatus(status: string) {
+        switch (status) {
+            case 'on': {
+                let new_list = this.task_list.filter(
+                    (tache) => !tache.is_terminate,
+                );
+                this.task_list.splice(0, this.task_list.length); // Clear la liste
+                new_list.map((tache) => this.task_list.push(tache));
+                break;
+            }
+            case 'finished': {
+                let new_list = this.task_list.filter(
+                    (tache) => tache.is_terminate,
+                );
+                this.task_list.splice(0, this.task_list.length); // Clear la liste
+                new_list.map((tache) => this.task_list.push(tache));
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+
+    private filterByPriority(priority: string) {
+        switch (priority) {
+            case 'weak': {
+                let new_list = this.task_list.filter(
+                    (tache) => tache.priority === 1,
+                );
+                this.task_list.splice(0, this.task_list.length); // Clear la liste
+                new_list.map((tache) => this.task_list.push(tache));
+                break;
+            }
+            case 'medium': {
+                let new_list = this.task_list.filter(
+                    (tache) => tache.priority === 2,
+                );
+                this.task_list.splice(0, this.task_list.length); // Clear la liste
+                new_list.map((tache) => this.task_list.push(tache));
+                break;
+            }
+            case 'high': {
+                let new_list = this.task_list.filter(
+                    (tache) => tache.priority === 3,
+                );
+                this.task_list.splice(0, this.task_list.length); // Clear la liste
+                new_list.map((tache) => this.task_list.push(tache));
+                break;
+            }
+            default: {
+                break;
+            }
+        }
+    }
+}
