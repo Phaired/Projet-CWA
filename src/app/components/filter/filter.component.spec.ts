5932091471c3508778c9cfc465e02506aea59068
Yoan Dusoleil <y.dusoleil@outlook.com>
2023-11-07 14:09:36 +0100

    prettier :/

--- src/app/components/filter/filter.component.spec.ts
+++ src/app/components/filter/filter.component.spec.ts
@@ -3,19 +3,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FilterComponent } from './filter.component';
 
 describe('FilterComponent', () => {
-  let component: FilterComponent;
-  let fixture: ComponentFixture<FilterComponent>;
+    let component: FilterComponent;
+    let fixture: ComponentFixture<FilterComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FilterComponent]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [FilterComponent],
+        });
+        fixture = TestBed.createComponent(FilterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
     });
-    fixture = TestBed.createComponent(FilterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
