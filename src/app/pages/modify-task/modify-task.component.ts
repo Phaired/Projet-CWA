a5932f7a2f5a93794010a79980aed58de8b8c716
Yoan Dusoleil <y.dusoleil@outlook.com>
2023-11-14 09:37:53 +0100

    Fix de la modif d'une tâche

--- src/app/pages/modify-task/modify-task.component.ts
+++ src/app/pages/modify-task/modify-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Tache } from '../../model/Tache';
 import { LocalStorageRepositoryService } from '../../repository/local-storage-repository.service';
 
@@ -7,8 +7,8 @@ import { LocalStorageRepositoryService } from '../../repository/local-storage-re
     templateUrl: './modify-task.component.html',
     styleUrls: ['./modify-task.component.css'],
 })
-export class ModifyTaskComponent {
-    @Input() tache: Tache = new Tache(
+export class ModifyTaskComponent implements OnInit {
+    @Input() tacheToModify: Tache = new Tache(
         1,
         'test',
         new Date(),
@@ -18,6 +18,7 @@ export class ModifyTaskComponent {
         false,
         'red',
     );
+    tache: Tache = Tache.fromTache(this.tacheToModify);
     @Output() onConfirm: EventEmitter<number> = new EventEmitter<number>();
 
     constructor(
@@ -32,10 +33,25 @@ export class ModifyTaskComponent {
             this.localStorageRepositoryService
                 .getLocalStorageRepository()
                 .updateTacheById(this.tache.id, this.tache);
+
+            //update des infos de la tâche, c'est pas ouf mais c'est le plus simple à faire sans refaire tout le code
+            this.tacheToModify.id = this.tache.id;
+            this.tacheToModify.intitule = this.tache.intitule;
+            this.tacheToModify.date_creation = this.tache.date_creation;
+            this.tacheToModify.date_fin = this.tache.date_fin;
+            this.tacheToModify.description = this.tache.description;
+            this.tacheToModify.priority = this.tache.priority;
+            this.tacheToModify.is_terminate = this.tache.is_terminate;
+            this.tacheToModify.color = this.tache.color;
+
             this.onConfirm.emit(0);
             return true;
         }
         return false;
+    }
+
+    ngOnInit() {
+        this.tache = Tache.fromTache(this.tacheToModify);
     }
 
     colorToPriority(priority: number): string {
