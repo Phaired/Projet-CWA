572a9f2467fa2336645e8ed382a22d1d41235cb0
Maxime <98154358+Maxime-Cllt@users.noreply.github.com>
2023-11-14 14:16:18 +0100

    #61
    Co-Authored-By: Yoan Dusoleil <73293479+Yaon-C2H8N2@users.noreply.github.com>

--- src/app/repository/LocalStorageRepository.ts
+++ src/app/repository/LocalStorageRepository.ts
@@ -1,18 +1,24 @@
 import { Tache } from '../model/Tache';
 
+// Interface pour exporter les tâches et des données supplémentaires
 export interface IExportTaches {
     tasks: Array<Tache>;
     other: object;
 }
 
+// Classe LocalStorageRepository pour gérer les tâches dans le stockage local
 export class LocalStorageRepository {
+    // Constantes pour définir l'ordre de tri
     public static readonly ORDER_ASC: number = 1;
     public static readonly ORDER_DESC: number = -1;
 
+    // Clé pour stocker les tâches dans le stockage local
     private readonly key: string;
 
+    // Le constructeur initialise la clé et configure le stockage local si nécessaire
     constructor() {
         this.key = 'CADMIUM_TASKS';
+        // Vérifie si la clé est présente dans le stockage local, initialise si ce n'est pas le cas
         window.localStorage.getItem(this.key) === null
             ? window.localStorage.setItem(
                   this.key,
@@ -24,6 +30,7 @@ export class LocalStorageRepository {
             : null;
     }
 
+    // Méthode pour obtenir toutes les tâches du stockage local
     public getAllTaches(): Array<Tache> {
         return (
             JSON.parse(
@@ -32,17 +39,13 @@ export class LocalStorageRepository {
         ).tasks;
     }
 
+    // Méthode pour obtenir le dernier ID utilisé pour une tâche
     public getLastId(): number {
         const taches: Array<Tache> = this.getAllTaches();
         return taches.length > 0 ? taches[taches.length - 1].id : 0;
     }
 
-    /**
-     * @deprecated
-     * This methode is no longer supported.
-     * <p> Use FilterComponent.orderBy(orderType, order) instead.
-     * @param order
-     */
+    // Méthode pour obtenir toutes les tâches par date de création
     public getAllTachesByDateCreation(order: number): Array<Tache> {
         const taches: Array<Tache> = this.getAllTaches();
         return taches.sort((a: Tache, b: Tache) => {
@@ -52,12 +55,7 @@ export class LocalStorageRepository {
         });
     }
 
-    /**
-     * @deprecated
-     * This methode is no longer supported.
-     * <p> Use FilterComponent.orderBy(orderType, order) instead.
-     * @param order
-     */
+    // Méthode pour obtenir toutes les tâches par priorité
     public getAllTachesByPriority(order: number): Array<Tache> {
         const taches: Array<Tache> = this.getAllTaches();
         return taches.sort((a: Tache, b: Tache) => {
@@ -65,6 +63,7 @@ export class LocalStorageRepository {
         });
     }
 
+    // Méthode pour sauvegarder une tâche
     public saveTache(tache: Tache): void {
         const taches: Array<Tache> = this.getAllTaches();
         taches.push(tache);
@@ -77,6 +76,7 @@ export class LocalStorageRepository {
         );
     }
 
+    // Méthode pour supprimer une tâche
     public deleteTache(tache: Tache): void {
         console.log(tache);
         const taches: Array<Tache> = this.getAllTaches();
@@ -98,10 +98,11 @@ export class LocalStorageRepository {
                 } as IExportTaches),
             );
         } else {
-            console.log('Task not found in the array.');
+            console.log('Tâche non trouvée dans le tableau.');
         }
     }
 
+    // Méthode pour supprimer toutes les tâches
     public deleteAllTaches(): void {
         const taches: Array<Tache> = this.getAllTaches();
         taches.splice(0, taches.length);
@@ -114,11 +115,13 @@ export class LocalStorageRepository {
         );
     }
 
+    // Méthode pour obtenir une tâche par son ID
     public getTacheById(id: number): Tache {
         const taches: Array<Tache> = this.getAllTaches();
         return taches.find((tache: Tache) => tache.id === id) as Tache;
     }
 
+    // Méthode pour mettre à jour une tâche
     public updateTache(previousTache: Tache, newTache: Tache): void {
         const taches: Array<Tache> = this.getAllTaches();
         const index: number = taches.findIndex(
@@ -134,6 +137,7 @@ export class LocalStorageRepository {
         );
     }
 
+    // Méthode pour mettre à jour une tâche par son ID
     public updateTacheById(id: number, newTache: Tache): void {
         const previousTache: Tache = this.getTacheById(id);
         this.updateTache(previousTache, newTache);
